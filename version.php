bbe96d8ab2f75a4666333faadddf33998f613ff8
Juan Leyva <juanleyvadelgado@gmail.com>
2014-04-10 13:58:47 +0200

    New language strings: Download files and videos

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014012402;
+$plugin->version  = 2014012403;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '1.0';
+$plugin->release  = '1.1';
