10616bb21abae075ff9cf160968d3e70bf334fb9
Juan Leyva <juanleyvadelgado@gmail.com>
2014-07-11 16:35:41 +0200

    Correct release version number

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version  = 2014012405;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '1.4.4';    // This should be the latest Mobile app version
+$plugin->release  = '1.4.5';    // This should be the latest Mobile app version
