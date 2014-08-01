51872f29eaa84c7722c2cfd7d369e040801e4669
Juan Leyva <juanleyvadelgado@gmail.com>
2014-08-01 15:52:00 +0200

    Language strings for version 1.6

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014012405;
+$plugin->version  = 2014012406;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '1.4.5';    // This should be the latest Mobile app version
+$plugin->release  = '1.4.6';    // This should be the latest Mobile app version
