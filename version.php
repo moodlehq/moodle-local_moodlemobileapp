ec791769112e41f0de309f5e7e8ac160ce64c2ee
Juan Leyva <juanleyvadelgado@gmail.com>
2014-11-08 13:59:13 +0100

    Language strings for version 1.8 - Last minute strings

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014012409;
+$plugin->version  = 2014012410;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = '1.8';    // This should be the latest Mobile app version
