a203e8b7c68e428f6e023181f49386874256c2f2
Juan Leyva <juanleyvadelgado@gmail.com>
2015-01-13 13:23:40 +0100

    Language strings for version 1.11 (bump version numbers)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014012412;
+$plugin->version  = 2014012413;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '1.10';    // This should be the latest Mobile app version
+$plugin->release  = '1.11';    // This should be the latest Mobile app version
