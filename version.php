704aea42250874c1a9a4e8481b7d3ee7c70ebfd1
Juan Leyva <juanleyvadelgado@gmail.com>
2017-07-27 00:25:45 +0200

    Strings for Moodle Mobile 3.3.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014012436;
+$plugin->version  = 2014012437;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '1.330';    // This should be the latest Mobile app version
+$plugin->release  = '1.331';    // This should be the latest Mobile app version
 $plugin->component = 'local_moodlemobileapp';
