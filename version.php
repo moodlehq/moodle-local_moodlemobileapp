0874a81803d97048b2baa4a0f8772abf2e5c4e4c
Travis CI User <travis@example.org>
2019-11-06 14:56:53 +0000

    Update lang files from 3.7.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019091700;
+$plugin->version = 2019110600;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.7.1';    // This should be the latest Mobile app version
