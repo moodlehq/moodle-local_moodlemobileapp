d006758e408b9ab7a8e834437ab70bf36ae2b569
David Castellón <dcaste99@gmail.com>
2019-12-13 11:47:53 +0100

    MOBILE-3297 behat: Behat basic test for survey

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2019121000;
+$plugin->version = 2019110600;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.8.0-rc';    // This should be the latest Mobile app version
+$plugin->release = '3.7.1';    // This should be the latest Mobile app version
 $plugin->component = 'local_moodlemobileapp';
