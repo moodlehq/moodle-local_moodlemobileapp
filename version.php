c12ac1c00c26f9872f94f300429921b570b829fe
Pau Ferrer Ocaña <crazyserver@gmail.com>
2020-06-23 16:20:53 +0200

    Update lang files from 3.9.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2020060900;
+$plugin->version = 2020062300;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.9.0-rc';    // This should be the latest Mobile app version
+$plugin->release = '3.9.0';    // This should be the latest Mobile app version
 $plugin->component = 'local_moodlemobileapp';
