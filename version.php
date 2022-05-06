31d7dcd9e094110c16faa5cd8745a062fdaae68a
Pau Ferrer Ocaña <crazyserver@gmail.com>
2022-05-06 15:03:13 +0200

    Recover corrupt lang file

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2022042200;
+$plugin->version = 2022050600;
 $plugin->requires = 2013111800; // Require Moodle version (2.6).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '4.0.0';    // This should be the latest Mobile app version
