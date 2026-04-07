734a9c4b1f4d5ac89ce3a62969f013530762ce80
Pau Ferrer Ocaña <crazyserver@gmail.com>
2026-04-07 22:06:41 +0200

    Update lang files from v5.2.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026020900;
+$plugin->version = 2026040700;
 $plugin->requires = 2018051700; // Require Moodle version (3.5).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '5.1.1';    // This should be the latest Mobile app version
+$plugin->release = '5.2.0-beta';    // This should be the latest Mobile app version
 $plugin->component = 'local_moodlemobileapp';
